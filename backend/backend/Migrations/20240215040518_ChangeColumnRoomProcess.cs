9f2dc9480786cc915ad1cd8453d137d759dfc9c0
Huỳnh Công Khanh <65171477+Khanhpro1250@users.noreply.github.com>
2024-02-15 11:09:11 +0700

    migrate : change column room process

---
+++ backend/backend/Migrations/20240215040518_ChangeColumnRoomProcess.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    public partial class ChangeColumnRoomProcess : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndDate",
+                schema: "HouseSchema",
+                table: "RoomProcess");
+
+            migrationBuilder.DropColumn(
+                name: "StartDate",
+                schema: "HouseSchema",
+                table: "RoomProcess");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Action",
+                schema: "HouseSchema",
+                table: "RoomProcess",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Action",
+                schema: "HouseSchema",
+                table: "RoomProcess");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndDate",
+                schema: "HouseSchema",
+                table: "RoomProcess",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartDate",
+                schema: "HouseSchema",
+                table: "RoomProcess",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
--- backend/backend/Models/Entities/Rooms/RoomProcess.cs
+++ backend/backend/Models/Entities/Rooms/RoomProcess.cs
@@ -7,9 +7,8 @@ public class RoomProcess
     public Guid Id { get; set; }
     public Guid RoomId { get; set; }
     public Guid CustomerId { get; set; }
-    public DateTime StartDate { get; set; }
-    public DateTime? EndDate { get; set; }
     public string Status { get; set; }
+    public string Action { get; set; }
     public string Note { get; set; }
     public Room Room { get; set; }
     public Customer Customer { get; set; }
